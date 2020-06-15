5c78262fb2ad284ca50ac37c14ebaff8bdee43f0
inonoa <himura.kennsinn.degozaru@gmail.com>
2020-06-15 19:55:28 +0900

    スクロールします(マウスでは)

---
+++ Assets/Scripts/Scroller.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class Scroller : MonoBehaviour
+{
+    [SerializeField] float upBound = 0;
+    [SerializeField] float downBound = -1000;
+    [SerializeField] float force = 10;
+
+    [SerializeField] [Range(0, 1)] float smoothness = 0.9f;
+
+    [SerializeField] [ReadOnly] float wheelRot = 0;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        wheelRot = Mathf.Lerp(wheelRot, Input.GetAxisRaw("Mouse ScrollWheel"), 1 - smoothness);
+        Vector3 pos = transform.localPosition;
+        pos.y = Mathf.Clamp(pos.y - wheelRot * force, downBound, upBound);
+        transform.localPosition = pos;
+    }
+}
