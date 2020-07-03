878877969a06ca28ba8b6b1bd49fe7c360f9d007
inonoa <himura.kennsinn.degozaru@gmail.com>
2020-07-04 05:57:06 +0900

    Explodeで巻き込む

---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class Bomb : MonoBehaviour
+{
+    [SerializeField] NodeMover[] nodesTmp;
+    [Button] void ExplodeTest() => Explode(nodesTmp);
+
+    float radius;
+
+    void Start()
+    {
+        radius = transform.localScale.x / 2;
+    }
+
+    public void Explode(NodeMover[] nodes){
+
+        foreach(NodeMover node in nodes){
+            if(Vector3.Distance(transform.position, node.transform.position) < radius){
+                //Boardに通知しないといけなそう
+                node.Vanish();
+            }
+        }
+    }
+}
