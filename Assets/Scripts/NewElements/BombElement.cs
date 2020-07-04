39df886326f9382b284011118aab7f5d6efe5186
inonoa <himura.kennsinn.degozaru@gmail.com>
2020-07-05 00:33:46 +0900

    ダイアログも出るよ

---
+++ Assets/Scripts/NewElements/BombElement.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+[CreateAssetMenu(fileName = "BombElement", menuName = "ScriptableObjects/BombElement", order = 5)]
+public class BombElement : NewElement
+{
+
+    protected override bool ExistIn(StageData data){
+        return data.Rows.Any(row => row.Nodes.Contains(NodeType.BombCyan));
+    }
+}
